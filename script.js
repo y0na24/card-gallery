93e9bec5fb066120833610445c65370420607780
Matvey <110357779+y0na24@users.noreply.github.com>
2022-10-27 19:06:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
 
-for (let slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('slide--active');
-    })
-};
+  const slides = document.querySelectorAll('.slide');
+  
+  slides[activeSlide].classList.add('slide--active');
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-      slide.classList.remove('slide--active')
-    })
-};
+  for (let slide of slides) {
+      slide.addEventListener('click', () => {
+          clearActiveClasses()
+          slide.classList.add('slide--active');
+      })
+  };
+  
+  function clearActiveClasses() {
+      slides.forEach((slide) => {
+        slide.classList.remove('slide--active')
+      })
+  };
+}
+slidesPlugin();
